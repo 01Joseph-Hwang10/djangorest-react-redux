9be491b0ee17355ec89e692c24d2aab9af811dbd
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-01-08 16:43:54 +0900

    fixing all

--- frontend/src/components/ToDoCard.js
+++ frontend/src/components/ToDoCard.js
@@ -4,9 +4,10 @@ function ToDoCard(props) {
 
     return (
         <div className="toDoCard border p-3 flex center">
-            <div className="w-1/2 flex justify-start">
-                <h1>{props.todos_name} | </h1>
-                <h1>{props.todos_important.toString()}</h1>
+            <div className="w-1/2 flex justify-start items-center">
+                <div><h1>{props.todos_name}</h1></div>
+                <div className="mx-1 h-full w-1 bg-black"></div>
+                <div><h1>{props.todos_important.toString()}</h1></div>
             </div>
             <div className="w-1/2 flex justify-end">
                 <h1>{props.created_username}</h1>
