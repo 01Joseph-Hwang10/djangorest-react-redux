90db509a5c772fc6a32ed01a5cbcda55a3465107
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-01-10 00:42:21 +0900

    folder form change

--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views as core_view
-
-app_name = "core"
-
-urlpatterns = [
-    path("", core_view.TemporaryView.as_view(), name="temporary")
-]
--- backend/users/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-
-
-class SignUpForm(UserCreationForm):
-
-    username = forms.EmailField(
-        label="Email"
-    )
